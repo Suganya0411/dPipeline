cc4593870fc8343883a4d46b31189c7a99facac3
Suganya0411 <149463817+Suganya0411@users.noreply.github.com>
2023-11-06 11:43:25 +0530

    Add files via upload

---
+++ src/test/java/testHello/SsimpleProgram.java
@@ -0,0 +1,13 @@
+package testHello;
+
+public class SsimpleProgram {
+
+	public static void main(String[] args) {
+	
+			System.out.println("Hello World");
+			System.out.println("Hello Wipro");
+			System.out.println("Hello Jenkins");
+			
+			
+		}
+}
